8e6dfd5bf070ce4ada151e0f6f03793ca0a2554a
LucidCodes <57482590+bloms11@users.noreply.github.com>
2023-04-27 10:43:19 +0000

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,20 +3,9 @@ import ProductList from './components/ProductList';
 
 export default async function Home() {
 
-  const res = await fetch(
-    `https://fakestoreapi.com/products`, 
-    {
-      next: {
-        revalidate: 30,
-      }
-    })
-
-  const data = await res.json();
-  // console.log(data)
   return (
     <div className="w-[90%] mx-auto ">
       <Header />
-      {/* {data ? (<ProductList />) : JSON.stringify(data)} */}
       <ProductList/>
     </div> 
 )
